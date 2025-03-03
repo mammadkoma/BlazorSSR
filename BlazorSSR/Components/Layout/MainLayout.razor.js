d9af1ed3d3dd904962589c4ad68ae1770f09b886
105615691 <105615691@omidtech-co.local>
2025-03-03 13:07:59 +0330

    event.data

--- BlazorSSR/Components/Layout/MainLayout.razor.js
+++ BlazorSSR/Components/Layout/MainLayout.razor.js
@@ -7,8 +7,8 @@ export function onUpdate() {
     for (let i = 0; i < inputs.length; i++) {
         inputs[i].addEventListener('input', (event) => {
             inputs[i].setAttribute('maxLength', '10');
-            //if (isNaN(event.data) && event.data != '/')
-            //    inputs[i].value = inputs[i].value.slice(0, -1);
+            if (event.data && isNaN(event.data) && event.data != '/')
+                inputs[i].value = inputs[i].value.slice(0, -1);
         });
     }
 }
